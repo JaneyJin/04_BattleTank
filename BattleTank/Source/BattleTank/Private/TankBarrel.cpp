142904a51af18d46e540879ed691343a83878e85
JaneyJin <jane_jys@hotmail.com>
2019-07-08 20:57:44 -0400

    BT42 Using Clamp() to Limit Values.cmproj

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,13 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
-
+#include "Engine/World.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed) {
 	
 	// UE_LOG(LogTemp, Warning, TEXT(" Barrel-Elevate called at speed %f"), RelativeSpeed)
 
+
+	// Clamp relative speed between -1 to 1
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+
 	// Move the barrel the right amout this frame
 	// Given a max elevation speed, and the frame time
+	// Returns the frame delta time in seconds, adjusted by time dilation.
+	auto ElevationChange = MaxDegreesPerSecond * RelativeSpeed * GetWorld()->DeltaTimeSeconds;
+	// Unclamped new elevation
+	// Rotation of the component relative to its parent, it will stop until hit the maximum ptich rotator value (90 degrees)
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	// Clamp can be used to tell it what specific type of thing to clamp; float can be ignore, it will get some implicity type conversion
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+
+	SetRelativeRotation(FRotator(Elevation,0,0));
 }
 
